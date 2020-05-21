84453f04c2aceed332838d32afb22df9eefa9a1b
FuroreX <preritprarudrakar@gmail.com>
2020-05-22 00:15:14 +0530

    second commit

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const path = require("path");
 
 const filePath = path.join(__dirname, "typeDefs.gql");
 const typeDefs = fs.readFileSync(filePath, "utf-8");
-
+const resolvers = require("./resolvers");
+ 
 
 require("dotenv").config({ path: "variables.env" });
 const User = require("./models/User");
