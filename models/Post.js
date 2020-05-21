7aaff39bad50f03106ed1bb0810838589c34518b
FuroreX <preritprarudrakar@gmail.com>
2020-05-21 20:53:48 +0530

    first commit

---
+++ models/Post.js
@@ -0,0 +1,51 @@
+ const mongoose = require('mongoose');
+
+ const PostSchema = new mongoose.Schema({
+     title:{
+         type: String,
+         required: true
+     },
+     imageUrl: {
+         type: String,
+         required: true
+     },
+     categeories: {
+         type: [String],
+         required: true
+     },
+     description: {
+         type: String,
+         required: true
+     },
+     createdDate: {
+         type: Date,
+         default: Date.now
+     },
+     likes: {
+         type: Number,
+         default: 0
+     },
+     createdBy: {
+         type: mongoose.Schema.Types.ObjectId,
+         required: true,
+         ref: 'User'
+     },
+     messages:[{
+         messaageBody: {
+             type: String,
+             required: true
+         },
+         messageDate: {
+             type: Date,
+             default: Date.now
+         },
+         messageUser: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User'
+         }
+     }]
+ });
+
+
+ module.export = mongoose.model('Post', PostSchema);
