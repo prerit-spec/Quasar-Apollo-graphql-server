cb29fa307461add1de940d8e5dde5bb49050e5fa
FuroreX <preritprarudrakar@gmail.com>
2020-05-22 18:49:07 +0530

    addPost mutation done :)

--- models/Post.js
+++ models/Post.js
@@ -14,9 +14,9 @@
          required: true
      },
      description: {
-         type: String,
-         required: true
-     },
+        type: String,
+        required: true
+      },
      createdDate: {
          type: Date,
          default: Date.now
@@ -28,7 +28,7 @@
      createdBy: {
          type: mongoose.Schema.Types.ObjectId,
          required: true,
-         ref: 'User'
+         ref: "User"
      },
      messages:[{
          messaageBody: {
@@ -42,10 +42,10 @@
          messageUser: {
             type: mongoose.Schema.Types.ObjectId,
             required: true,
-            ref: 'User'
+            ref: "User"
          }
      }]
  });
 
 
- module.export = mongoose.model('Post', PostSchema);
+ module.exports = mongoose.model("Post", PostSchema);
--- resolvers.js
+++ resolvers.js
@@ -1,10 +1,24 @@
-module.export = {
+module.exports = {
     Query: {
-       getUser: () => null
+        getUser: () => null
     },
     Mutation: {
+        addPost: async (
+            _,
+            { title, imageUrl, categories, description, creatorId },
+            { Post }
+        ) => {
+            const newPost = await new Post({
+                title,
+                imageUrl,
+                categories,
+                description,
+                createdBy: creatorId
+            }).save();
+            return newPost;
+        },
         signupUser: async (_, { username, email, password }, { User }) => {
-            const user = await User.findOne({ username  });
+            const user = await User.findOne({ username });
             if (user) {
                 throw new Error('User already exists');
             }
